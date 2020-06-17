7cb2adfb92490c514651e3950313b3234cfa75ad
Marat <imanovmarat@gmail.com>
2020-06-17 16:50:04 +0300

    на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,28 @@ let popup = document.querySelector ('.popup'),
     popupCloseBtn = popup.querySelector('.popup__close-btn'),
     name = document.querySelector('.profile__name'),
     position = document.querySelector('.profile__position'),
-    nameInput =  popup.querySelector('.popup__edit-name'),
-    jobInput = popup.querySelector('.popup__edit-position')
+    nameInput =  popup.querySelector('.popup__input_field_name'),
+    jobInput = popup.querySelector('.popup__field_field_position');
 
 function popupToggle() {
-  popup.classList.toggle ('popup_opened')
-  nameInput.value = name.textContent
-  jobInput.value = position.textContent
+  if (!popup.classList.contains('popup_opened')) {
+    nameInput.value = name.textContent;
+    jobInput.value = position.textContent;
+  }
+  popup.classList.toggle('popup_opened');
+
 }
 
-nameEditBtn.addEventListener('click', popupToggle)
-popupCloseBtn.addEventListener('click', popupToggle)
+nameEditBtn.addEventListener('click', popupToggle);
+popupCloseBtn.addEventListener('click', popupToggle);
 
 
-let formElement = popup.querySelector('.popup__container')
+let formElement = popup.querySelector('.popup__container');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    name.textContent = nameInput.value
-    position.textContent = jobInput.value
+    name.textContent = nameInput.value;
+    position.textContent = jobInput.value;
 
 popupToggle();
 }
