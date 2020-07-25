10d1f964bd640c612404ba4d6f206bdd400e47d1
Marat <imanovmarat@gmail.com>
2020-07-25 15:39:26 +0300

    refact: убран импорт config из FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,9 +1,8 @@
-import {config} from './utils.js';
-
 export default class FormValidator {
   constructor(config, formElement) {
+    this._config = config;
     this._formElement = formElement;
-    this._inputList = Array.from(this._formElement.querySelectorAll(config.inputSelector));
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._config.inputSelector));
     this._setEventListener();
   }
 
@@ -14,13 +13,13 @@ export default class FormValidator {
   }
 
   _toggleButtonState() {
-    this._submitButton = this._formElement.querySelector(config.submitButtonSelector);
+    this._submitButton = this._formElement.querySelector(this._config.submitButtonSelector);
 
     if (this._hasInvalidInput(this._inputList)) {
-      this._submitButton.classList.add(config.inactiveButtonClass);
+      this._submitButton.classList.add(this._config.inactiveButtonClass);
       this._submitButton.setAttribute('disabled', true);
     } else {
-      this._submitButton.classList.remove(config.inactiveButtonClass);
+      this._submitButton.classList.remove(this._config.inactiveButtonClass);
       this._submitButton.removeAttribute('disabled');
     }
   }
@@ -28,17 +27,17 @@ export default class FormValidator {
   _showInputError(inputElement, errorMessage) {
     const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`);
 
-    inputElement.classList.add(config.inputErrorClass);
+    inputElement.classList.add(this._config.inputErrorClass);
     errorElement.textContent = errorMessage;
-    errorElement.classList.add(config.errorClass);
+    errorElement.classList.add(this._config.errorClass);
   }
 
   _hideInputError(inputElement) {
     const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`);
 
-    inputElement.classList.remove(config.inputErrorClass);
+    inputElement.classList.remove(this._config.inputErrorClass);
     errorElement.textContent = '';
-    errorElement.classList.remove(config.errorClass);
+    errorElement.classList.remove(this._config.errorClass);
   }
 
   _checkInputValidity(inputElement) {
